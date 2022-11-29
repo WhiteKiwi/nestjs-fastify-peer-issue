ad4abf9601e839178f6386591e2c749c7ae947bb
kiwi-jang <kiwi.jang@toss.im>
2022-11-29 15:46:09 +0900

    Reproduce fastify peer issue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastify from 'fastify';
 
 import { AppModule } from './app.module';
 import { Env } from './config/env';
@@ -8,8 +13,11 @@ import { TransformResponseInterceptor } from './core/interceptors';
 import { setupSwagger } from './core/setup';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-
+	const fastifyInstance = fastify();
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(fastifyInstance),
+	);
 	setupSwagger(app);
 
 	app.setGlobalPrefix('api', { exclude: ['docs'] });
